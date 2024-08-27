c069b5d46cbe8b3cc972391bf80322fd43b678e0
NguyenMinhDuc163 <NguyenMinhDuc163@users.noreply.github.com>
2024-08-27 16:39:31 +0000

    deploy: 48ed273ad8419ba3e4b2243abba9156c3e3d1a94

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3226594214"
+    serviceWorkerVersion: "1705986541"
   }
 });
